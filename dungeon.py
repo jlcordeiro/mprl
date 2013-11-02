82b598500e1acb1d994515addeeb06833196db05
Joao Cordeiro <jlcordeiro@gmail.com>
2013-11-02 18:50:09 +0000

    added auto generating map

--- dungeon.py
+++ dungeon.py
@@ -2,6 +2,9 @@ import libtcodpy as libtcod
 
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
@@ -33,13 +36,15 @@ class Rect:
       return (self.x1 <= other.x2 and self.x2 >= other.x1 and
               self.y1 <= other.y2 and self.y2 >= other.y1)
 
-
 class LevelModel:
    def __init__(self):
       #fill map with "unblocked" tiles
       self.grid = [[ Tile(True)
             for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
+
+      self.rooms = []
+      self.num_rooms = 0
 
 class LevelView:
    def __init__(self,model):
@@ -58,16 +63,53 @@ class LevelView:
 class LevelController:
    def __init__(self):
       self.model = LevelModel()
-
-      #create two rooms
-      room1 = Rect(20, 15, 10, 15)
-      room2 = Rect(50, 15, 10, 15)
-
-      self.create_room(room1)
-      self.create_room(room2)
-      self.create_h_tunnel(25, 55, 23)
-
       self.view = LevelView(self.model)
+                                
+      for r in range(MAX_ROOMS):
+         #random width and height
+         w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+         h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+         #random position without going out of the boundaries of the map
+         x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+         y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+    
+         #"Rect" class makes rectangles easier to work with
+         new_room = Rect(x, y, w, h)
+    
+         #run through the other rooms and see if they intersect with this one
+         failed = False
+         for other_room in self.model.rooms:
+            if new_room.intersect(other_room):
+               failed = True
+               break
+                                                   
+         if not failed:
+            #this means there are no intersections, so this room is valid
+            #"paint" it to the map's tiles
+            self.create_room(new_room)
+    
+            #center coordinates of new room, will be useful later
+            (new_x, new_y) = new_room.center()
+    
+            if self.model.num_rooms > 0:
+               #connect all rooms but the first to the previous room with a tunnel
+
+               #center coordinates of previous room
+               (prev_x, prev_y) = self.model.rooms[self.model.num_rooms-1].center()
+    
+               #draw a coin (random number that is either 0 or 1)
+               if libtcod.random_get_int(0, 0, 1) == 1:
+                  #first move horizontally, then vertically
+                  self.create_h_tunnel(prev_x, new_x, prev_y)
+                  self.create_v_tunnel(prev_y, new_y, new_x)
+               else:
+                  #first move vertically, then horizontally
+                  self.create_v_tunnel(prev_y, new_y, prev_x)
+                  self.create_h_tunnel(prev_x, new_x, new_y)
+    
+            #finally, append the new room to the list
+            self.model.rooms.append(new_room)
+            self.model.num_rooms += 1
 
    def create_room(self,room):
       #go through the tiles in the rectangle and make them passable
@@ -82,6 +124,6 @@ class LevelController:
          self.model.grid[x][y].block_sight = False
 
    def create_v_tunnel(self, y1, y2, x):
-      for x in range(min(y1, y2), max(y1, y2) + 1):
+      for y in range(min(y1, y2), max(y1, y2) + 1):
          self.model.grid[x][y].blocked = False
          self.model.grid[x][y].block_sight = False
