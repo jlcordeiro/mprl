bcc3eaefd342dc1f61159d78c9f20442372ede8f
joao <joao@gimo.co.uk>
2014-02-06 17:47:26 +0000

    removed uid

--- models/objects.py
+++ models/objects.py
@@ -1,18 +1,7 @@
 
-last_uid = 0
-
-
-def get_uid():
-    global last_uid
-    last_uid += 1
-    return last_uid
-
-
 class ObjectModel(object):
     def __init__(self, name, x, y, blocks):
         self.x = x
         self.y = y
         self.name = name
         self.blocks = blocks
-
-        self.uid = str(get_uid())
