51932c025d92527a789d5d6b57bb6aceb961bf7a
Joao Cordeiro <jlcordeiro@gmail.com>
2014-04-13 16:35:06 +0100

    main level is 1 room only and empty

--- models/dungeon.py
+++ models/dungeon.py
@@ -63,8 +63,8 @@ def create_room_connection(room1, room2, mode="center"):
     return (h_tunnel, v_tunnel)
 
 
-class Level:
-    def __init__(self, name):
+class BasicLevel(object):
+    def __init__(self, name, n_rooms):
         #fill map with "unblocked" tiles
         self.tiles = [[Tile(True)
                        for y in range(MAP_HEIGHT)]
@@ -90,22 +90,18 @@ class Level:
         # of them. Format: (position, char, nturns)
         self.temp_artifacts = []
 
-        self._generate()
-        self._compute_fov()
-
-    def __add_room(self, room):
-        self.rooms.append(room)
-        self.num_rooms += 1
-
-    def __dig_room(self, room):
+    def dig_room(self, room):
         #go through the tiles in the rectangle and make them passable
-
         tl = room.top_left
         br = room.bottom_right
         for x in range(tl.x, br.x):
             for y in range(tl.y, br.y):
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
+
+    def add_room(self, room):
+        self.rooms.append(room)
+        self.num_rooms += 1
 
     def is_blocked(self, pos):
         x, y = pos
@@ -118,72 +114,7 @@ class Level:
         return (self.get_monster_in_pos(pos) is not None)
 
     def is_in_fov(self, pos):
-        return libtcod.map_is_in_fov(self.fov_map, pos[0], pos[1]) 
-
-    def __place_monsters_in_room(self, room):
-        #choose random number of monsters
-        num_monsters = random.randint(0, MAX_ROOM_MONSTERS)
-
-        for i in range(num_monsters):
-            #choose random spot for this monster
-            x, y = room.get_random_point().coords
-
-            if not self.is_blocked((x, y)):
-                monster = MonsterFactory(x, y)
-                self.monsters.append(monster)
-
-    def __place_items_in_room(self, room):
-        #choose random number of items
-        num_items = libtcod.random_get_int(0, 0, MAX_ROOM_ITEMS)
-
-        for i in range(num_items):
-            #choose random spot for this item
-            x, y = room.get_random_point().coords
-
-            #only place it if the tile is not blocked
-            if not self.is_blocked((x, y)):
-                item = ItemFactory(x, y)
-                self.items.append(item)
-
-    def __build_complete_room(self):
-        #random width and height
-        w = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-        h = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-        #random position without going out of the boundaries of the map
-        x = random.randint(1, MAP_WIDTH - w - 1)
-        y = random.randint(1, MAP_HEIGHT - h - 1)
-
-        room = Room(x, y, w, h)
-
-        #check if there are no intersections, so this room is valid
-        intersection = next((r for r in self.rooms if r.intersects(room)), None)
-        if intersection is None:
-            self.__dig_room(room)
-            self.__add_room(room)
-
-            self.__place_items_in_room(room)
-            self.__place_monsters_in_room(room)
-
-    def _generate(self):
-        # build rooms
-        for r in range(MAX_ROOMS):
-            self.__build_complete_room()
-
-        # connect rooms
-        for room in self.rooms:
-            if room.connected:
-                continue
-
-            unconnected = [r for r in self.rooms if not r.connected and r != room]
-
-            if len(unconnected) > 0:
-                closest = min(unconnected, key = room.distance_to_rect)
-
-                (h_tunnel, v_tunnel) = create_room_connection(room, closest, "random")
-                self.__dig_room(h_tunnel)
-                self.__dig_room(v_tunnel)
-
-            room.connected = True
+        return libtcod.map_is_in_fov(self.fov_map, pos[0], pos[1])
 
     def random_unblocked_pos(self):
         #choose random spot
@@ -250,6 +181,101 @@ class Level:
                     self.tiles[x][y].explored = True
 
 
+class Town(BasicLevel):
+    def __init__(self):
+        super(Town, self).__init__("Town", 1)
+
+        self.__build_empty_room()
+        self._compute_fov()
+
+    def __build_empty_room(self):
+        #random width and height
+        w = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        #random position without going out of the boundaries of the map
+        x = random.randint(1, MAP_WIDTH - w - 1)
+        y = random.randint(1, MAP_HEIGHT - h - 1)
+
+        room = Room(x, y, w, h)
+        self.dig_room(room)
+        self.add_room(room)
+
+
+class Level(BasicLevel):
+    def __init__(self, name, n_rooms):
+        super(Level, self).__init__(name, n_rooms)
+        self.__generate(n_rooms)
+        self._compute_fov()
+
+    def __generate(self, n_rooms):
+        # build rooms
+        for r in range(n_rooms):
+            self.__build_complete_room()
+
+        # connect rooms
+        for room in self.rooms:
+            if room.connected:
+                continue
+
+            unconnected = [r for r in self.rooms if not r.connected and r != room]
+
+            if len(unconnected) > 0:
+                closest = min(unconnected, key = room.distance_to_rect)
+
+                (h_tunnel, v_tunnel) = create_room_connection(room, closest, "random")
+                self.dig_room(h_tunnel)
+                self.dig_room(v_tunnel)
+
+            room.connected = True
+
+    def __place_monsters_in_room(self, room):
+        #choose random number of monsters
+        num_monsters = random.randint(0, MAX_ROOM_MONSTERS)
+
+        for i in range(num_monsters):
+            #choose random spot for this monster
+            x, y = room.get_random_point().coords
+
+            if not self.is_blocked((x, y)):
+                monster = MonsterFactory(x, y)
+                self.monsters.append(monster)
+
+    def __place_items_in_room(self, room):
+        #choose random number of items
+        num_items = libtcod.random_get_int(0, 0, MAX_ROOM_ITEMS)
+
+        for i in range(num_items):
+            #choose random spot for this item
+            x, y = room.get_random_point().coords
+
+            #only place it if the tile is not blocked
+            if not self.is_blocked((x, y)):
+                item = ItemFactory(x, y)
+                self.items.append(item)
+
+    def __build_complete_room(self):
+        #random width and height
+        w = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = random.randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        #random position without going out of the boundaries of the map
+        x = random.randint(1, MAP_WIDTH - w - 1)
+        y = random.randint(1, MAP_HEIGHT - h - 1)
+
+        room = Room(x, y, w, h)
+
+        #check if there are no intersections, so this room is valid
+        intersection = next((r for r in self.rooms if r.intersects(room)), None)
+        if intersection is None:
+            self.dig_room(room)
+            self.add_room(room)
+
+            self.__place_items_in_room(room)
+            self.__place_monsters_in_room(room)
+
+
+
+
+
 # M (Main level)
 # |
 # |--- Branch 1 -- (...)
@@ -265,7 +291,7 @@ class Dungeon:
         self.levels = {}
 
         # main level
-        main_level = Level("Town")
+        main_level = Town()
 
         self.levels[main_level.name] = main_level
         self.current_level = self.levels[main_level.name]
@@ -285,7 +311,7 @@ class Dungeon:
         for l in xrange(0, NUM_LEVELS):
             level_name = branch_name + " (" + str(l+1)  + ")"
 
-            new_level = Level(level_name)
+            new_level = Level(level_name, MAX_ROOMS)
             to_pos = new_level.random_unblocked_pos()
 
             levels[level_name] = new_level
